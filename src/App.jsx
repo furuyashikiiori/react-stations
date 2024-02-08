614199ad7708c0a6b5f8a2e1091a0b25dbd94a17
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-02-08 13:16:58 +0900

    APIによってランダムな犬の画像を取得して表示

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,26 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/stbernard/n02109525_10908.jpg',
   )
+
+  const change = async () => {
+    const data = await fetch('https://dog.ceo/api/breeds/image/random')
+    const img = await data.json()
+    return img.message
+  }
+  const ChangeClick = async () => {
+    const imageUrl = await change()
+    setDogUrl(imageUrl)
+  }
   return (
     <div>
       <header>
         <h1>初めてのReact</h1>
       </header>
-      <p>犬の画像を表示するサイトです！</p>
-      <img src={dogUrl} alt="サイトから持ってきた犬の画像" />
+      <p>犬の画像を表示するサイト！</p>
+      <button onClick={ChangeClick}>更新</button>
+      <p>
+        <img src={dogUrl} alt="サイトから持ってきた犬の画像" />
+      </p>
     </div>
   )
 }
