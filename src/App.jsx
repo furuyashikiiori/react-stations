8e1764a69febbd26d4b342cc819b2f5497947d1c
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-02-08 12:07:04 +0900

    State関数を使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 // DO NOT DELETE
 
 import './App.css'
+import React, { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/stbernard/n02109525_10908.jpg',
+  )
   return (
     <div>
       <header>
         <h1>初めてのReact</h1>
       </header>
       <p>犬の画像を表示するサイトです！</p>
-      <img
-        src="https://images.dog.ceo/breeds/stbernard/n02109525_10908.jpg"
-        alt="サイトから持ってきた犬の画像"
-      />
+      <img src={dogUrl} alt="サイトから持ってきた犬の画像" />
     </div>
   )
 }
